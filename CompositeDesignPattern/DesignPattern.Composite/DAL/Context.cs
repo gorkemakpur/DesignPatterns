6c1fa84f54bc5cd84db08564e675e5399a0c87b9
gorkemakpur <gorkemakpur@gmail.com>
2023-10-17 23:20:27 +0300

    Proje eklendi,  veritabanı duzenlendi

---
+++ CompositeDesignPattern/DesignPattern.Composite/DAL/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DesignPattern.Composite.DAL
+{
+    public class Context :DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)//veritabanını ayağa kaldırmak için kullanıyoruz
+        {
+            optionsBuilder.UseSqlServer("server=LAPTOP-SVROI9OR\\SQLEXPRESS;" +
+                "initial catalog=CompositeDesignPattern; integrated security=true;");
+        }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
